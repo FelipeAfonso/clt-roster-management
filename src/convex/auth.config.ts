4556c4b84ebeb02ece76c7477f5957a8ac50403d
Felipe Afonso <fmunhozafonso@gmail.com>
2026-02-18 16:35:40 -0300

    fix: auth issues

--- src/convex/auth.config.ts
+++ src/convex/auth.config.ts
@@ -1,14 +1,7 @@
 const clientId = process.env.WORKOS_CLIENT_ID;
 
-const authConfig = {
+export default {
 	providers: [
-		{
-			type: 'customJwt' as const,
-			issuer: `https://api.workos.com/`,
-			algorithm: 'RS256' as const,
-			jwks: `https://api.workos.com/sso/jwks/${clientId}`,
-			applicationID: clientId
-		},
 		{
 			type: 'customJwt' as const,
 			issuer: `https://api.workos.com/user_management/${clientId}`,
@@ -17,5 +10,3 @@ const authConfig = {
 		}
 	]
 };
-
-export default authConfig;
